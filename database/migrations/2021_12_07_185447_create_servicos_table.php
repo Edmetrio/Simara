eeb92cac77c0b217f7a4217b90645dafd050e4f8
Edmétrio J <edmetrio06@gmail.com>
2021-12-08 06:58:00 +0000

    add servicos

---
+++ database/migrations/2021_12_07_185447_create_servicos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('servico', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('titulo')->nullable();
+            $table->string('subtitulo')->nullable();
+            $table->string('foto')->nullable();
+            $table->string('icon')->nullable();
+            $table->string('descricao')->nullable();
+            $table->string('estado')->default('on')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('servico');
+    }
+}
