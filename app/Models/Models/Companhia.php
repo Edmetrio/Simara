c12ea16f9c7df74a52cd9724f8c216766b4a8e76
Edmétrio J <edmetrio06@gmail.com>
2021-12-10 13:26:59 +0000

    model companhia

---
+++ app/Models/Models/Companhia.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Uuid;
+
+class Companhia extends Model
+{
+    use HasFactory, Uuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+
+    protected $table = 'companhia';
+    protected $fillable = ['nome','icon','estado'];
+}
