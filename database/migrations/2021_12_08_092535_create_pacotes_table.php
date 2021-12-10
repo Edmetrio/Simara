0d4d5d8eb67266d494f864566deea79d30336d8c
Edmétrio J <edmetrio06@gmail.com>
2021-12-10 13:28:52 +0000

    bd pacotes

---
+++ database/migrations/2021_12_08_092535_create_pacotes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePacotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pacote', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('titulo')->nullable();
+            $table->string('subtitulo')->nullable();
+            $table->string('icon')->nullable();
+            $table->string('reserva')->nullable();
+            $table->string('descricao')->nullable();
+            $table->string('preco')->nullable();
+            $table->string('cidade')->nullable();
+            $table->string('estado')->default('on')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pacote');
+    }
+}
