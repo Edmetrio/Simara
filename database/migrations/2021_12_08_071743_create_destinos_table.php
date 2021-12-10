11ee85079d472d0cf57e7c73e8bb8c1d1b882a50
Edmétrio J <edmetrio06@gmail.com>
2021-12-10 13:27:58 +0000

    base de dados destino

---
+++ database/migrations/2021_12_08_071743_create_destinos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDestinosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('destino', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('nome')->nullable();
+            $table->string('foto')->nullable();
+            $table->string('icon')->nullable();
+            $table->string('estado')->default('on')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('destino');
+    }
+}
