73ebc44234cc18e14004a6358511346436bd2abe
Edmétrio J <edmetrio06@gmail.com>
2021-12-13 11:22:34 +0000

    select servico

--- resources/views/createPost.blade.php
+++ resources/views/createPost.blade.php
@@ -29,6 +29,7 @@
                 @endif
                     @csrf
                     <div class="form-row">
+
                         <div class="form-group col-md-6">
                             <label for="inputCity" class="col-form-label">Título</label>
                             <input type="text" class="form-control" placeholder="Título" name="titulo" value="{{$post->titulo ?? ''}}">
@@ -36,6 +37,15 @@
                         <div class="form-group col-md-6">
                             <label for="inputCity" class="col-form-label">SubTítulo</label>
                             <input type="text" class="form-control" placeholder="SubTítulo" name="subtitulo" value="{{$post->subtitulo ?? ''}}">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="inputCity" class="col-form-label">Serviço</label>
+                            <select name="servico_id" class="form-control">
+                                <option value="">Seleccione o Serviço</option>
+                                @foreach($servico as $s)
+                                <option value="{{$s->id}}">{{$s->titulo}}</option>
+                                @endforeach
+                            </select>
                         </div>
                         <div class="form-group col-md-6">
                             <label for="inputCity" class="col-form-label">Ícone</label>
