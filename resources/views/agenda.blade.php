29e81e7f497aacf61072b966a5bc58e42b58eef6
Edmétrio J <edmetrio06@gmail.com>
2022-02-08 09:44:34 +0200

    balde email

--- resources/views/agenda.blade.php
+++ resources/views/agenda.blade.php
@@ -20,29 +20,45 @@
     </div>
     <div class="inner-main common-spacing container">
         <!-- form -->
-        <form class="twocol-form" action="#">
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Opss!</strong> Algum problema com seu formulário<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+        @if ($message = Session::get('status'))
+        <div>
+            <p class="alert alert-success" class="table p-field p-col-12 p-md-6 table-striped" style="text-align: center;">{{ $message }}</p>
+        </div>
+        @endif
+        <form class="twocol-form" action="{{ route('agenda.store')}}" method="POST">
+            @csrf
             <div class="row">
                 <div class="col-md-12">
                     <div class="top-box">
-                        <span class="holder height" style="text-align: center; font-size: 50px; font-weight: bold;">Agendar</span>
+                        <span class="holder height" style="text-align: center; font-size: 50px; font-weight: bold;">AGENDAR</span>
                     </div>
                     <div class="form-holder">
                         <div class="wrap">
                             <div class="hold">
                                 <label for="name">Nome</label>
-                                <input type="text" id="nome" class="form-control" />
+                                <input type="text" name="nome" class="form-control" />
                             </div>
                             <div class="hold">
                                 <label for="uname1">E-mail</label>
-                                <input type="email" id="email" class="form-control" />
+                                <input type="email" name="email" class="form-control" placeholder="simaratravel@teledata.mz" />
                             </div>
 
                             <div class="holder">
-                                <label for="select-region">Seleccione a Região</label>
+                                <label for="select-region">Seleccione o Serviço</label>
                                 <div class="select-holder">
-                                    <select class="trip-select trip-select-v2 region" name="region" id="select-region">
+                                    <select class="trip-select trip-select-v2 region" name="regiao" id="select-region">
                                         @foreach($servico as $s)
-                                        <option value="{{$s->id}}">{{$s->titulo}}</option>
+                                        <option value="{{$s->titulo}}">{{$s->titulo}}</option>
                                         @endforeach
                                     </select>
                                 </div>
@@ -50,8 +66,9 @@
 
                             <div class="hold">
                                 <label for="reg-pass">Orçamento</label>
-                                <input type="text" id="reg-pass" class="form-control" placeholder="12.500M,00MT" />
+                                <input type="text" name="orcamento" class="form-control" placeholder="0.0MT" />
                             </div>
+
                             <div class="btn-hold" style="width: 12px;">
                                 <button type="submit" class="btn btn-default">
                                     AGENDAR
