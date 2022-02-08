6b85e267d5a15108148b3877138352718ecbbc68
Edmétrio J <edmetrio06@gmail.com>
2022-02-08 09:47:19 +0200

    resource detail

---
+++ resources/views/emails/TestMail.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Solicitação de Servico</title>
+</head>
+<body>
+    <h2>{{$detail['regiao']}}</h2><hr>
+    <p><strong>Nome: </strong>{{$detail['nome']}}</p>
+    <p><strong> E-mail: </strong>{{$detail['email']}}</p>
+    <p><strong> Região: </strong>{{$detail['regiao']}}</p>
+    <p><strong>Orçamento: </strong>{{$detail['orcamento']}}</p>
+    <p>Muito Obrigado</p>
+    <p>Criado por <a href="https://firsteducation.edu.mz/">FirstTech</a></p>
+</body>
+</html>
