71942979aa99f6b401425749976850b67f561991
Edmétrio J <edmetrio06@gmail.com>
2022-03-28 08:49:34 +0200

    table cliente

---
+++ database/migrations/2022_02_08_074110_create_clientes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cliente', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('nome')->nullable();
+            $table->string('data')->nullable();
+            $table->string('descricao')->nullable();
+            $table->string('telefone')->nullable();
+            $table->string('email')->unique();
+            $table->string('estado')->default('on')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cliente');
+    }
+}
